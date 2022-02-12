4a27569cef429eb26db30b6e322ac6fd719aa9ae
unknown <hellyeah1506@gmail.com>
2022-02-12 21:19:15 +0700

    update mo hình 3 khoi

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,11 @@
+//tính điểm chuẩn
+/*
+bước 1 : lấy value từ input
+bước 2 : tính tổng điểm và so sánh điều kiện với điểm chuẩn
+bước 3 : xuất kết quả ra
+*/
+         
+
 document.getElementById("ketQuaXt").onclick = function(){
     const number1 = document.getElementById("number1").value;
     const number2 = document.getElementById("number2").value;
@@ -21,49 +29,26 @@ document.getElementById("ketQuaXt").onclick = function(){
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// tính số KW Tiêu thụ
+/*
+bước 1: lấy thông tin người dùng
+        
+bước 2: tạo ra các biến KW_50, KW_50_100, KW_150,KW_200,KW_200_NEXT
+        tạo biến price
+        tính toán tiền điện tiêu thụ theo từng mốc
+        50kw = kwUseEL * KW_50;
+        50kw tiếp theo :50 *KW_50 + ((kwUseEL-50)*KW_50_100)
+        100kw kế :50 *KW_50 + (50 * KW_50_100) + ((kwUseEL-100) * KW_150);
+        150kw kế :50 *KW_50 + (50 * KW_50_100) +(50 * KW_150)+((kwUseEL-150) * KW_200);
+        còn lại :50 *KW_50 + (50 * KW_50_100) +(50 * KW_150)+(50 * KW_200)+((kwUseEL-200) * KW_200_NEXT);
+bước 3: xuất ra kết quả tên + số tiền phải trả
+*/
 
 const KW_50 = 500;
 const KW_50_100 = 650;
 const KW_150 = 850;
 const KW_200 = 1100;
 const KW_200_NEXT = 1300;
-
-
-
-
 document.getElementById("ketqua").onclick = function(){
     const kwUseEL = document.getElementById("KWtieuthu").value;
     let price = 0
